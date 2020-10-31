848b29c6ea4143aabbfbbb129b42584f23856ab1
= <lea.arturi@gmail.com>
2020-10-31 18:44:45 -0300

    Listo

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const routes = [
   {
@@ -16,8 +16,19 @@ const routes = [
     path: '/contador',
     name: 'Contador',
     component: () => import('../views/Contador.vue')
+  },
+  {
+    path: '/paises',
+    name: 'Paises',
+    component: () => import('../views/Paises.vue')
+  },
+  {
+    path: '/paises/:nombre',
+    name: 'Pais',
+    props: true,
+    component: () => import('../views/Pais.vue')
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ src/views/Pais.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>Detalle de Pais: {{ nombre }}</h1>
+
+        <p v-for="(pais, index) in arrayData" :key="index">
+            {{ pais.name }} - {{ pais.region }}
+        </p>
+
+  </div>
+</template>
+
+<script>
+import { useRoute } from 'vue-router';
+import { useFetch } from '../hooks/useFetch';
+
+export default {
+    props: ['nombre'],
+    setup(props) {
+        const nombreParams = useRoute();
+
+        const { arrayData } = useFetch(`https://restcountries.eu/rest/v2/name/${nombreParams.params.nombre}`);
+        return { arrayData };
+    }
+
+}
+</script>
+
